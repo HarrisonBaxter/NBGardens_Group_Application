03354ac10f297d1f81a59c2771ae5f4b1759b7a0
Paul Tan <paultan38@gmail.com>
2016-07-13 00:04:59 +0100

    Added an failure test to EmployeeTest, Modified Inventory Item Test, Added PurchaseOrderLineTest and PurchaseOrderList Test

--- src/test/scala/EmployeeTest.scala
+++ src/test/scala/EmployeeTest.scala
@@ -9,6 +9,12 @@ class EmployeeTest extends FlatSpec with Matchers {
   it should "Find user" taggedAs FindWorker in {
     WorkerList.findWorker("Harrison").isEmpty shouldEqual false
   }
+
+  it should "Worker Should not Exist" taggedAs NonexistentWorker in{
+    WorkerList.findWorker("Taylor").isEmpty shouldEqual true
+  }
 }
 
 object FindWorker extends Tag("com.test.FindWorker")
+
+object NonexistentWorker extends Tag("com.test.NonexistentWorker")
